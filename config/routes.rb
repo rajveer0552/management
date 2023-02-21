4dee29990f112a415cc5f0ab59104e8d06c3ea20
rajveersharma <rajveersharma0552@gmail.com>
2023-02-21 16:41:37 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/create'
+  get 'user/show'
+  get 'user/delete'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
